fe251519c0f2d233e48b85611ef51385d38c88ec
Doloment <doloment@internet.ru>
2021-09-10 21:47:19 +0700

    Try to make a sauron eye using particles

--- mods/lord/lord_classes/effects.lua
+++ mods/lord/lord_classes/effects.lua
@@ -3,7 +3,7 @@ effects.decrease_hp = {}
 effects.sauron_eye  = {}
 
 local dmg_timer = 5
---local eye_t = 10
+local eye_timer = 10
 
 local function decrease_hp()
 	for _, player in pairs(effects.decrease_hp) do
@@ -12,6 +12,26 @@ local function decrease_hp()
 		end
 	end
 	minetest.after(dmg_timer, decrease_hp)
+end
+
+local function sauron_eye()
+	for _, player in pairs(effects.sauron_eye) do
+		local ppos = player:get_pos()
+		local pvel = player:get_player_velocity()
+		if pvel == {x=0, y=0, z=0} then pvel = player:get_look_dir() end
+		local obj = minetest.add_entity()
+		--[[minetest.add_particle({
+			pos = {x=ppos.x, y=ppos.y+1.5, z=ppos.z},
+			velocity = pvel,
+			expirationtime = 5,
+			size = 25,
+			collisiondetection = false,
+			vertical = false,
+			texture = "sauron_eye.png",
+			playername = player:get_player_name(),
+		})]]
+	end
+	minetest.after(eye_timer, sauron_eye)
 end
 
 -- не нагружаем сервер, а проверяем только раз в секунду
@@ -30,9 +50,11 @@ local function check_player_pos(dtime)
 		local player_name = player:get_player_name()
 		if player_areas[area_id] and races.get_race(player_name) == "hobbit" then
 			effects.decrease_hp[player_name] = player
+			effects.sauron_eye[player_name] = player
 		else
-			if effects.decrease_hp[player_name] then
+			if effects.decrease_hp[player_name] or effects.sauron_eye[player_name] then
 				effects.decrease_hp[player_name] = nil
+				effects.sauron_eye[player_name] = nil
 			end
 		end
 	end
@@ -49,5 +71,5 @@ if not area_id then
 end
 
 minetest.after(dmg_timer, decrease_hp)
---minetest.after(eye_t, sauron_eye)
+minetest.after(eye_timer, sauron_eye)
 minetest.register_globalstep(check_player_pos)
